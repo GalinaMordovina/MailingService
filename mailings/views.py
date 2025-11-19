cd303ea4a770301da5b7f9dd2257ac08a34599dd
Galina Mordovina <glukoloid@gmail.com>
2025-11-19 22:07:47 +0500

    CRUD для клиентов

--- mailings/views.py
+++ mailings/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
-# from .models import Mailing, Client  # пока их нет, позже создам
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Client # Mailing пока его нет, позже создам
+
 
 class HomeView(TemplateView):
     template_name = 'mailings/home.html'
@@ -11,3 +16,35 @@ class HomeView(TemplateView):
         context['mailings_active'] = 0
         context['clients_unique'] = 0
         return context
+
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'mailings/client_list.html'
+    context_object_name = 'clients'
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = 'mailings/client_detail.html'
+    context_object_name = 'client'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailings/client_form.html'
+    success_url = reverse_lazy('mailings:client_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'mailings/client_form.html'
+    success_url = reverse_lazy('mailings:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'mailings/client_confirm_delete.html'
+    success_url = reverse_lazy('mailings:client_list')
