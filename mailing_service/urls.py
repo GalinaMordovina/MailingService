8ccd52181743cef819606f4f255b277fd1a0b4cc
Galina Mordovina <glukoloid@gmail.com>
2025-11-20 20:45:22 +0500

    USERS: регистрация, вход, выход, профиль пользователя

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -7,5 +7,6 @@ from mailings.views import HomeView  # добавляем
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),  # главная
-    path('mailings/', include('mailings.urls', namespace='mailings')),  # подключу позже
+    path('mailings/', include('mailings.urls', namespace='mailings')),
+    path('users/', include('users.urls')),
 ]
